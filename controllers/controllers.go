f0c67002b268788329bf4ae51f69fbd2c8f8b243
TyHafen <tyhafen@gmail.com>
2023-02-25 23:58:39 -0800

    controllers models

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,1 +1,30 @@
 package controllers
+
+import "github.com/gin-gonic/gin"
+
+func HashPassword(password string) string {
+
+}
+
+func VerifyPassword(userPassword string, givenPassword string) (bool, string) {
+
+}
+
+func Signup() gin.HandlerFunc {
+
+}
+
+func Login() gin.HandlerFunc {
+
+}
+
+func ProductViewerAdmin() gin.HandlerFunc {
+
+}
+
+func SearchProducts() gin.HandlerFunc {
+
+}
+func SearchProductsByQuery() gin.HandlerFunc {
+
+}
--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
+	"log"
 	"os"
 
 	"github.com/gin-gonic/gin"
-	"github.com/go-chi/chi/middleware"
 	"github.com/tyhafen.ecommerce-go/controllers"
 	"github.com/tyhafen.ecommerce-go/database"
 	"github.com/tyhafen.ecommerce-go/middleware"
@@ -22,5 +22,12 @@ func main() {
 
 	routes.UserRoutes(router)
 	router.Use(middleware.Authentication())
+
+	router.GET("/addtocart", app.AddToCart())
+	router.GET("removeitem", app.RemoveItem())
+	router.GET("/cartcheckout", app.BuyFromCart())
+	router.GET("/instantbuy", app.InstantBuy())
+
+	log.Fatal(router.Run(":" + port))
 
 }
